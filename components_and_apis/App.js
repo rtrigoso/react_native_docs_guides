c02b57369ad584d23e614f7eba6830c0782e6d9b
ren trigoso <renzotrigoso@gmail.com>
2019-08-20 09:56:46 -0400

    components_and_apis first commit

---
+++ components_and_apis/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightyellow'
+  }
+});
+
+const App = () => {
+  return (
+    <View style={styles.container} >
+    </View>
+  )
+}
+
+export default App;
---
+++ components_and_apis/android/app/src/main/java/com/components_and_apis/MainActivity.java
@@ -0,0 +1,15 @@
+package com.components_and_apis;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "components_and_apis";
+    }
+}
