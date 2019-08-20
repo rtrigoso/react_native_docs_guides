ac933c694656b79ba1efc4f66d96ad0392e74f25
ren trigoso <renzotrigoso@gmail.com>
2019-08-20 10:03:47 -0400

    added picker

--- components_and_apis/App.js
+++ components_and_apis/App.js
@@ -9,19 +9,51 @@
 import React, {Component} from 'react';
 import {
   StyleSheet,
-  View
+  View,
+  Picker
 } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'lightyellow'
+  },
+  child: {
+    flex:1,
+    backgroundColor: 'pink',
+    margin: 15,
+    borderColor: 'white'
   }
 });
+
+class Content extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      language: 'java'
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <Picker
+          selectedValue={this.state.language}
+          onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
+          style={styles.child} >
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="Javascript" value="js" />
+        </Picker>
+      </View>
+    )
+  }
+}
 
 const App = () => {
   return (
     <View style={styles.container} >
+      <Content />      
     </View>
   )
 }
